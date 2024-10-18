977e733bf34c4d8828d38057a38dcb961d6f3ec0
Yousef Saeed <uosyph@gmail.com>
2024-10-18 04:05:25 +0300

    Update return to top button

--- landing_page/js/app.js
+++ landing_page/js/app.js
@@ -103,7 +103,14 @@ window.addEventListener('scroll', (event) => {
 
 // Begin Go Up Button
 // Create the div element for the button
-footer.insertAdjacentHTML("beforebegin", `<div id="return_top" style="cursor: pointer;">↑</div>`);
+footer.insertAdjacentHTML("beforebegin",
+  `<div id="return_top" style="cursor: pointer;">
+      <svg height="1.2em" id="arrow" fill="#fff" viewBox="0 0 512 512">
+        <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z">
+        </path>
+      </svg>
+  </div>`
+);
 
 // Scroll to the top of the page on button click
 document.getElementById("return_top").addEventListener('click', function () {
