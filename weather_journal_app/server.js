ab3e576a45380bba65224ff4799c18ef8ffee090
Yousef Saeed <uosyph@gmail.com>
2024-10-23 05:13:08 +0300

    Setup the server

--- weather_journal_app/server.js
+++ weather_journal_app/server.js
@@ -1,9 +1,11 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +13,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Setup Server.
+const port = 3000;
 
-// Setup Server
+// Spin up the server
+const server = app.listen(port, () => {
+    console.log(`Server is listing on port: ${port}`);
+});
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// GET route that returns the projectData
+app.get('/all', (req, res) => {
+    res.send(projectData);
+});
+
+// POST route that adds incoming data to projectData
+app.post('/clientAddedData', (req, res) => {
+    projectData = req.body;
+    res.send(projectData);
+});
