ba12382b4db4a7b7bf6db9f5d5b6f98eecf7cc98
Yousef Saeed <uosyph@gmail.com>
2024-10-29 00:51:29 +0300

    Update packages

--- articles_nlp/webpack.dev.js
+++ articles_nlp/webpack.dev.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,7 +18,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        }
+            }
         ]
     },
     plugins: [
@@ -27,17 +27,14 @@ module.exports = {
             filename: "./index.html",
         }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
             dry: true,
-            // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
     ],
     devServer: {
         port: 3000,
-        allowedHosts: 'all'
+        allowedHosts: ['all'],
     }
-}
+};
